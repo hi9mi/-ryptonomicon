ef58fa32888368d013e598d24cd3cd7519ed7788
Roman <4elove4ek40@gmail.com>
2022-11-29 19:58:07 +0600

    refactor: ticker card and filters components

--- src/app.vue
+++ src/app.vue
@@ -10,64 +10,25 @@
       />
       <template v-if="tickers.length">
         <hr class="w-full border-t border-gray-600 my-4" />
-        <div>
-          <button
-            v-if="page > 1"
-            @click="page = page - 1"
-            class="my-4 mx-2 inline-flex items-center py-2 px-4 border border-transparent shadow-sm text-sm leading-4 font-medium rounded-full text-white bg-gray-600 hover:bg-gray-700 transition-colors duration-300 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-500"
-          >
-            Назад
-          </button>
-          <button
-            v-if="hasNextPage"
-            @click="page = page + 1"
-            class="my-4 mx-2 inline-flex items-center py-2 px-4 border border-transparent shadow-sm text-sm leading-4 font-medium rounded-full text-white bg-gray-600 hover:bg-gray-700 transition-colors duration-300 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-500"
-          >
-            Вперед
-          </button>
-          <div>Фильтр: <input v-model="filter" /></div>
-        </div>
+        <ticker-filters
+          @prev-page="page -= 1"
+          @next-page="page += 1"
+          :hasNextPage="hasNextPage"
+          :hasPrevPage="page > 1"
+          :currentPage="Number(page)"
+          @filter-change="filter = $event"
+          :filter="filter"
+        />
         <hr class="w-full border-t border-gray-600 my-4" />
         <dl class="mt-5 grid grid-cols-1 gap-5 sm:grid-cols-3">
-          <div
+          <ticker-card
             v-for="t in paginatedTickers"
             :key="t.name"
-            @click="selectTicker(t)"
-            :class="{
-              'border-4': selectedTicker === t,
-              'bg-red-100': !t.valid,
-              'bg-white': t.valid,
-            }"
-            class="overflow-hidden shadow rounded-lg border-purple-800 border-solid cursor-pointer"
-          >
-            <div class="px-4 py-5 sm:p-6 text-center">
-              <dt class="text-sm font-medium text-gray-500 truncate">
-                {{ t.name }} - USD
-              </dt>
-              <dd class="mt-1 text-3xl font-semibold text-gray-900">
-                {{ formatPrice(t.price) }}
-              </dd>
-            </div>
-            <div class="w-full border-t border-gray-200"></div>
-            <button
-              @click.stop="deleteTicker(t)"
-              class="flex items-center justify-center font-medium w-full bg-gray-100 px-4 py-4 sm:px-6 text-md text-gray-500 hover:text-gray-600 hover:bg-gray-200 hover:opacity-20 transition-all focus:outline-none"
-            >
-              <svg
-                class="h-5 w-5"
-                xmlns="http://www.w3.org/2000/svg"
-                viewBox="0 0 20 20"
-                fill="#718096"
-                aria-hidden="true"
-              >
-                <path
-                  fill-rule="evenodd"
-                  d="M9 2a1 1 0 00-.894.553L7.382 4H4a1 1 0 000 2v10a2 2 0 002 2h8a2 2 0 002-2V6a1 1 0 100-2h-3.382l-.724-1.447A1 1 0 0011 2H9zM7 8a1 1 0 012 0v6a1 1 0 11-2 0V8zm5-1a1 1 0 00-1 1v6a1 1 0 102 0V8a1 1 0 00-1-1z"
-                  clip-rule="evenodd"
-                ></path></svg
-              >Удалить
-            </button>
-          </div>
+            :ticker="t"
+            :selected="t === selectedTicker"
+            @select-ticker="selectTicker($event)"
+            @delete-ticker="deleteTicker($event)"
+          />
         </dl>
         <hr class="w-full border-t border-gray-600 my-4" />
       </template>
@@ -86,6 +47,8 @@
 import CreateTickerForm from './components/create-ticker-form.vue';
 import TickerPriceGraph from './components/ticker-price-graph.vue';
 import PreLoader from './components/pre-loader.vue';
+import TickerFilters from './components/ticker-filters.vue';
+import TickerCard from './components/ticker-card.vue';
 import {
   subscribeToTicker,
   unsubscribeFromTicker,
@@ -98,6 +61,8 @@ export default {
     CreateTickerForm,
     TickerPriceGraph,
     PreLoader,
+    TickerFilters,
+    TickerCard,
   },
   data() {
     return {
@@ -185,6 +150,7 @@ export default {
       }
       ticker.price = price;
     },
+
     handleChangeTicker(event) {
       const ticker = event.target.value;
       this.error = false;
@@ -193,12 +159,6 @@ export default {
           availableTicker.toLowerCase().includes(ticker.toLowerCase())
         )
         .slice(0, 4);
-    },
-
-    formatPrice(price) {
-      price = price ?? '-';
-      if (price === '-') return price;
-      return price > 1 ? price.toFixed(2) : price.toPrecision(2);
     },
 
     createTicker(ticker) {
---
+++ src/components/ticker-filters.vue
@@ -0,0 +1,54 @@
+<template>
+  <div class="mt-2">
+    <button
+      v-if="hasPrevPage"
+      @click="$emit('prev-page')"
+      class="my-4 mx-2 inline-flex items-center py-2 px-4 border border-transparent shadow-sm text-sm leading-4 font-medium rounded-full text-white bg-gray-600 hover:bg-gray-700 transition-colors duration-300 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-500"
+    >
+      Назад
+    </button>
+    <button
+      v-if="hasNextPage"
+      @click="$emit('next-page')"
+      class="my-4 mx-2 inline-flex items-center py-2 px-4 border border-transparent shadow-sm text-sm leading-4 font-medium rounded-full text-white bg-gray-600 hover:bg-gray-700 transition-colors duration-300 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-500"
+    >
+      Вперед
+    </button>
+    <div>
+      Фильтр:
+      <input
+        :value="filter"
+        @input="$emit('filter-change', $event.target.value)"
+      />
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    currentPage: {
+      type: Number,
+      required: true,
+    },
+    hasNextPage: {
+      type: Boolean,
+      required: true,
+    },
+    hasPrevPage: {
+      type: Boolean,
+      required: true,
+    },
+    filter: {
+      type: String,
+      required: true,
+    },
+  },
+
+  emits: {
+    'prev-page': null,
+    'next-page': null,
+    'filter-change': (value) => typeof value === 'string',
+  },
+};
+</script>
