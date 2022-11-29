e53829489bb60502d276342535c106c287a3b436
Roman <4elove4ek40@gmail.com>
2022-11-29 17:54:55 +0600

    refactor: ticker price graph component

--- src/App.vue
+++ src/App.vue
@@ -1,31 +1,6 @@
 <template>
   <div class="container mx-auto flex flex-col items-center bg-gray-100 p-4">
-    <div
-      v-if="!availableTickers.length"
-      class="fixed w-100 h-100 opacity-80 bg-purple-800 inset-0 z-50 flex items-center justify-center"
-    >
-      <svg
-        class="animate-spin -ml-1 mr-3 h-12 w-12 text-white"
-        xmlns="http://www.w3.org/2000/svg"
-        fill="none"
-        viewBox="0 0 24 24"
-      >
-        <circle
-          class="opacity-25"
-          cx="12"
-          cy="12"
-          r="10"
-          stroke="currentColor"
-          stroke-width="4"
-        ></circle>
-        <path
-          class="opacity-75"
-          fill="currentColor"
-          d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
-        ></path>
-      </svg>
-    </div>
-
+    <pre-loader v-if="!availableTickers.length" />
     <div class="container">
       <create-ticker-form
         :suggestions="suggestions"
@@ -96,56 +71,21 @@
         </dl>
         <hr class="w-full border-t border-gray-600 my-4" />
       </template>
-
-      <section v-if="selectedTicker" class="relative">
-        <h3 class="text-lg leading-6 font-medium text-gray-900 my-8">
-          {{ selectedTicker.name }} - USD
-        </h3>
-        <div
-          ref="graph"
-          class="flex items-end border-gray-600 border-b border-l h-64"
-        >
-          <div
-            v-for="(bar, idx) in normalizedGraph"
-            :key="idx"
-            :style="{ height: `${bar}%` }"
-            class="bg-purple-800 border w-10"
-          ></div>
-        </div>
-        <button
-          @click="selectedTicker = null"
-          type="button"
-          class="absolute top-0 right-0"
-        >
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            xmlns:xlink="http://www.w3.org/1999/xlink"
-            xmlns:svgjs="http://svgjs.com/svgjs"
-            version="1.1"
-            width="30"
-            height="30"
-            x="0"
-            y="0"
-            viewBox="0 0 511.76 511.76"
-            style="enable-background: new 0 0 512 512"
-            xml:space="preserve"
-          >
-            <g>
-              <path
-                d="M436.896,74.869c-99.84-99.819-262.208-99.819-362.048,0c-99.797,99.819-99.797,262.229,0,362.048    c49.92,49.899,115.477,74.837,181.035,74.837s131.093-24.939,181.013-74.837C536.715,337.099,536.715,174.688,436.896,74.869z     M361.461,331.317c8.341,8.341,8.341,21.824,0,30.165c-4.16,4.16-9.621,6.251-15.083,6.251c-5.461,0-10.923-2.091-15.083-6.251    l-75.413-75.435l-75.392,75.413c-4.181,4.16-9.643,6.251-15.083,6.251c-5.461,0-10.923-2.091-15.083-6.251    c-8.341-8.341-8.341-21.845,0-30.165l75.392-75.413l-75.413-75.413c-8.341-8.341-8.341-21.845,0-30.165    c8.32-8.341,21.824-8.341,30.165,0l75.413,75.413l75.413-75.413c8.341-8.341,21.824-8.341,30.165,0    c8.341,8.32,8.341,21.824,0,30.165l-75.413,75.413L361.461,331.317z"
-                fill="#718096"
-                data-original="#000000"
-              ></path>
-            </g>
-          </svg>
-        </button>
-      </section>
+      <ticker-price-graph
+        v-if="selectedTicker"
+        :tickerName="selectedTicker.name"
+        :graph="graph"
+        @close-graph="selectedTicker = null"
+        @max-graph-elements="maxGraphElements = $event"
+      />
     </div>
   </div>
 </template>
 
 <script>
 import CreateTickerForm from './components/create-ticker-form.vue';
+import TickerPriceGraph from './components/ticker-price-graph.vue';
+import PreLoader from './components/pre-loader.vue';
 import {
   subscribeToTicker,
   unsubscribeFromTicker,
@@ -156,6 +96,8 @@ export default {
   name: 'App',
   components: {
     CreateTickerForm,
+    TickerPriceGraph,
+    PreLoader,
   },
   data() {
     return {
@@ -197,14 +139,6 @@ export default {
     }
   },
 
-  mounted() {
-    window.addEventListener('resize', this.calculateMaxGraphElements);
-  },
-
-  beforeUnmount() {
-    window.removeEventListener('resize', this.calculateMaxGraphElements);
-  },
-
   computed: {
     startIndex() {
       return (this.page - 1) * 6;
@@ -226,22 +160,6 @@ export default {
       return this.filteredTickers.length > this.endIndex;
     },
 
-    normalizedGraph() {
-      const maxValue = Math.max(...this.graph);
-      const minValue = Math.min(...this.graph);
-
-      if (
-        maxValue === minValue ||
-        (!isFinite(maxValue) && !isFinite(maxValue))
-      ) {
-        this.graph.map(() => 50);
-      }
-
-      return this.graph.map(
-        (price) => 5 + ((price - minValue) * 95) / (maxValue - minValue)
-      );
-    },
-
     pageStateOptions() {
       return {
         filter: this.filter,
@@ -251,14 +169,6 @@ export default {
   },
 
   methods: {
-    calculateMaxGraphElements() {
-      if (!this.$refs.graph) {
-        return;
-      }
-
-      this.maxGraphElements = Math.floor(this.$refs.graph.clientWidth / 38);
-    },
-
     updateTicker(tickerName, price, invalid) {
       const ticker = this.tickers.find((t) => t.name === tickerName);
       if (invalid) {
@@ -267,7 +177,6 @@ export default {
       }
 
       if (ticker === this.selectedTicker) {
-        console.log(this.maxGraphElements);
         this.graph.push(price);
 
         while (this.graph.length > this.maxGraphElements) {
@@ -287,6 +196,7 @@ export default {
     },
 
     formatPrice(price) {
+      price = price ?? '-';
       if (price === '-') return price;
       return price > 1 ? price.toFixed(2) : price.toPrecision(2);
     },
@@ -324,13 +234,15 @@ export default {
     },
 
     selectTicker(ticker) {
+      if (!ticker.valid) {
+        return;
+      }
       this.selectedTicker = ticker;
     },
   },
   watch: {
     selectedTicker() {
       this.graph = [];
-      this.$nextTick().then(this.calculateMaxGraphElements);
     },
     tickers() {
       localStorage.setItem('cryptonomicon-list', JSON.stringify(this.tickers));
